5f11a5ede70a218dc773c9db4fd8868cc346e812
shiroe66 <shiroeqky@gmail.com>
2022-06-23 23:44:41 +0500

    feat: move mouse

--- src/helpers/mouseEvents.ts
+++ src/helpers/mouseEvents.ts
@@ -1,11 +1,14 @@
 import { getMousePosition } from "../modules/getMousePosition"
+import { moveMousePosition } from "../modules/moveMousePosition"
 
 export const mouseEvents = (action: string) => {
   const [event, px] = action.split(" ")
 
-  switch (event) {
-    case "position":
-      getMousePosition()
-      break
+  if (event === "position") {
+    getMousePosition()
+  }
+
+  if (event === "up" || "down" || "right" || "left") {
+    moveMousePosition(event, Number(px))
   }
 }
---
+++ src/modules/moveMousePosition.ts
@@ -0,0 +1,20 @@
+import { getMousePos, moveMouse } from "robotjs"
+
+export const moveMousePosition = (event: string, px: number) => {
+  const { x, y } = getMousePos()
+
+  switch (event) {
+    case "up":
+      moveMouse(x, y - px)
+      break
+    case "down":
+      moveMouse(x, y + px)
+      break
+    case "left":
+      moveMouse(x - px, y)
+      break
+    case "right":
+      moveMouse(x + px, y)
+      break
+  }
+}
