7ae4a41d381222377086ad896cfb9d5191537dbc
shiroe66 <shiroeqky@gmail.com>
2022-06-24 19:49:40 +0500

    fix: send to front

--- src/modules/getMousePosition.ts
+++ src/modules/getMousePosition.ts
@@ -2,5 +2,5 @@ import { getMousePos } from "robotjs"
 
 export const getMousePosition = () => {
   const { x, y } = getMousePos()
-  console.log(`Mouse position: x: ${x}, y: ${y}`)
+  return `mouse_position ${x},${y}`
 }
