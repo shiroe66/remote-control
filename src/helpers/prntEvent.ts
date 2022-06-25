60b92da2c7169c000fc5b5a96aa2033f2c3145f4
shiroe66 <shiroeqky@gmail.com>
2022-06-25 16:19:31 +0500

    fix: switch case

--- src/helpers/prntEvent.ts
+++
@@ -1,6 +0,0 @@
-import { RawData, WebSocket } from "ws"
-import { getScreen } from "../modules/getScreen"
-
-export const prntEvent = (msg: RawData, ws: WebSocket) => {
-  getScreen(ws)
-}
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import "dotenv/config"
 import fs from "fs"
-import path from "path"
 import http from "http"
+import path from "path"
 import { WebSocketServer } from "ws"
-import { mouseEvents } from "./helpers/mouseEvents"
 import { drawEvents } from "./helpers/drawEvents"
-import { prntEvent } from "./helpers/prntEvent"
+import { mouseEvents } from "./helpers/mouseEvents"
+import { getScreen } from "./modules/getScreen"
 
 export const httpServer = http.createServer(function (req, res) {
   const __dirname = path.resolve(path.dirname(""))
@@ -40,7 +40,7 @@ wss.on("connection", (ws) => {
         drawEvents(msg, action, ws)
         break
       case "prnt":
-        prntEvent(msg, ws)
+        getScreen(ws)
         break
     }
   })
